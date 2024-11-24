e79729288a6a47e0082811c809507df244c009b1
mrkrishsoni <mrkrishsoni@gmail.com>
2024-11-24 17:10:54 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true, // Enables SWC compiler minification for better performance
-
   eslint: {
     ignoreDuringBuilds: true,
   },
